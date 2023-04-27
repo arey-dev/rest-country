5dcafb044a4cde64e29187ca4019ddd2ca4ce0c5
R.A Borja <raborja.dev@gmail.com>
2023-04-27 13:02:57 +0800

    ThemeToggle.jsx

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+export const ThemeToggle = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const html = document.documentElement;
+
+    if (isDarkMode) {
+      html.classList.add("dark");
+    } else {
+      html.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
+  const handleToggle = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  return (
+    <button onClick={handleToggle}>
+      {isDarkMode ? "Light Mode" : "Dark Mode"}
+    </button>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: {
+          background: "hsl(0, 0%, 98%)",
+          element: "hsl(0, 0%, 100%)",
+          text: "hsl(200, 15%, 8%)",
+          input: "hsl(0, 0%, 52%)",
+        },
+        dark: {
+          background: "hsl(207, 26%, 17%)",
+          element: "hsl(209, 23%, 22%)",
+          text: "hsl(0, 0%, 100%)",
+          input: "hsl(0, 0%, 100%)",
+        },
+      },
+      fontFamily: {
+        sans: ["Nunito Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
-
