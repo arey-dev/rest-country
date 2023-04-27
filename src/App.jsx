8d149beb098790576381508ce9b8f80a0c7adceb
R.A Borja <raborja.dev@gmail.com>
2023-04-27 14:07:43 +0800

    getAllCountries() & getCountryByName()

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,5 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p className="text-">
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
-}
+const App = () => {
+  return;
+};
 
 export default App;
---
+++ src/services/api.js
@@ -0,0 +1,35 @@
+export const getAllCountries = async () => {
+  const URL = "https://restcountries.com/v3.1/all";
+
+  try {
+    const response = await fetch(URL);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error!`, response.status);
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export const getCountryByName = async (countryName) => {
+  const url = `https://restcountries.com/v3.1/name/${countryName}`;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error!`, response.status);
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
