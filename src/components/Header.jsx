18cea6ce071ca495ae819c94adb65fe14237626e
R.A Borja <raborja.dev@gmail.com>
2023-05-01 12:38:07 +0800

    header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import { ThemeToggle } from "./ThemeToggle";
 
 export const Header = () => {
   return (
-    <header>
-      <div className="bg-light-element flex justify-between py-5 px-3 shadow-sm">
-        <h1 className="text-light-text font-extrabold">Where in the world?</h1>
+    <header className="bg-light-element shadow-sm dark:bg-dark-element">
+      <div className="container mx-auto flex justify-between py-5 px-3">
+        <h1 className="text-light-text font-extrabold dark:text-dark-text">Where in the world?</h1>
         <ThemeToggle />
       </div>
     </header>
--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -18,7 +18,7 @@ export const ThemeToggle = () => {
   };
 
   return (
-    <button className="font-semibold" onClick={handleToggle}>
+    <button className="font-semibold dark:text-dark-text" onClick={handleToggle}>
       {isDarkMode ? "Light Mode" : "Dark Mode"}
     </button>
   );
