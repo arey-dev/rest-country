9ee52d936c9989726958892c3aad5a751d4996c1
R.A Borja <raborja.dev@gmail.com>
2023-06-23 16:58:20 +0800

    spacing

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
+
 import { countriesLoader, countryLoader } from "../loaders";
 import { ErrorPage, SimpleSpinner } from "../components";
 import { lazy, Suspense } from "react";
+
 import Root from "./Root";
 import Home from "./Home";
-
 const CountryDetail = lazy(() => import("./CountryDetail"));
 
 export const router = createBrowserRouter(
