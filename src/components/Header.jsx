12da84f4dd7ecd8cf5621bada6bcfde3f29ff57e
R.A Borja <raborja.dev@gmail.com>
2023-06-23 17:01:34 +0800

    change link path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export const Header = ({ children }) => {
   return (
     <header className="bg-light-element shadow-sm dark:bg-dark-element mb-6 md:mb-12 transition duration-200 ease-in-out">
       <div className="container mx-auto flex justify-between py-6 px-3">
-        <Link to="/rest-country/">
+        <Link to="/">
           <h1 className="text-light-text sm:text-lg md:text-xl lg:text-2xl font-extrabold dark:text-dark-text">
             Where in the world?
           </h1>
