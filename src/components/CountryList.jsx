67d099124f31b707dc2a20970b5a156c2f87142c
R.A Borja <raborja.dev@gmail.com>
2023-06-23 16:57:01 +0800

    add useNavivgation

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable react/prop-types */
 import { CountryCard } from "./CountryCard";
+import { useNavigation } from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
 export const CountryList = ({ countries }) => {
+  const navigation = useNavigation();
   return (
-    <section>
+    <section className={navigation.state === "loading" ? "loading" : ""}>
       <h2 className="sr-only">Countries</h2>
       <div className="container mx-auto px-3 py-5 grid grid-cols-1 place-items-center sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-16">
         {countries.map((country) => (
