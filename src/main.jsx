00a71f00dfd6c80d6c3fda7e53c579f152c6dcb2
R.A Borja <raborja.dev@gmail.com>
2023-06-23 16:58:51 +0800

    add queryClient and queryClientProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import { RouterProvider } from "react-router-dom";
-import { router } from "./Routes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { router } from "./routes";
 import "./index.css";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 10,
+    },
+  },
+});
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools position="bottom-right" />
+    </React.StrictMode>
+  </QueryClientProvider>
 );
