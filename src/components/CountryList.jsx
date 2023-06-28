f7a85a3c01cfa8962435898e205bd2369f4fb5db
R.A Borja <raborja.dev@gmail.com>
2023-06-28 14:48:23 +0800

    add load more functionality

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,15 +1,28 @@
 /* eslint-disable react/prop-types */
+import { useState, useMemo } from "react";
 import { CountryCard } from "./CountryCard";
 import { useNavigation } from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
 export const CountryList = ({ countries }) => {
+  const [limit, setLimit] = useState(30);
   const navigation = useNavigation();
+
+  // limit countries to show
+  const countriesToShow = useMemo(
+    () => countries.slice(0, limit),
+    [countries, limit]
+  );
+
+  const handleLoadCountries = () => {
+    setLimit(limit + 50);
+  };
+
   return (
     <section className={navigation.state === "loading" ? "loading" : ""}>
       <h2 className="sr-only">Countries</h2>
-      <div className="container mx-auto px-3 py-5 grid grid-cols-1 place-items-center sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-16">
-        {countries.map((country) => (
+      <div className="container mx-auto px-3 py-5 grid grid-cols-1 place-items-center sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-16 min-h-full">
+        {countriesToShow.map((country) => (
           <CountryCard
             key={country.cca3}
             linkCode={country.cca3}
@@ -20,6 +33,14 @@ export const CountryList = ({ countries }) => {
             capital={country.capital}
           />
         ))}
+      </div>
+      <div className="container mx-auto flex justify-center p-10">
+        <button hidden={limit >= countries.length}
+          onClick={handleLoadCountries}
+          className="text-light-text bg-light-element shadow-md w-full sm:w-56 py-2 rounded-md ring-2 ring-inset hover:bg-light-background ring-light-element dark:ring-dark-element font-semibold dark:hover:bg-dark-background dark:text-dark-text dark:bg-dark-element"
+        >
+          Load More
+        </button>
       </div>
     </section>
   );
